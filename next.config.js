51707f9a40c9a721772dc72dbc700d6ddb05aa0b
Gildacio <gildacioribeiro@outlook.com.br>
2025-09-25 22:34:10 -0300

    fix(i18n): corrige import next-intl/plugin sem .js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 // frontend/next.config.js
-import createNextIntlPlugin from "next-intl/plugin.js";
+import createNextIntlPlugin from "next-intl/plugin";
 
-// Configura o next-intl para usar o request.ts
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
-
 };
 
 export default withNextIntl(nextConfig);
+
 
 
 
