55bd591005d293612b41cfc401d2e92ee3be3f73
Gildacio <gildacioribeiro@outlook.com.br>
2025-09-26 10:58:27 -0300

    fix: remover next-intl do next.config.js e resetar build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-// frontend/next.config.js
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+// next.config.js
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
 };
 
-export default withNextIntl(nextConfig);
-
+export default nextConfig;
 
 
 
